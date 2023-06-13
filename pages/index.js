f597ef0c5c8493e23988d68ce95c9673eba57e11
Akrur Neupane <113849010+neupaneakrur@users.noreply.github.com>
2023-06-13 09:26:17 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! V3.3</a>
+          Welcome to <a href="https://nextjs.org">Next.js! V4.0</a>
         </h1>
 
         <p className={styles.description}>
